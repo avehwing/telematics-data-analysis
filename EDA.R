e1e65ff41257f4c445d75ebef89c46c3ea784b23
Kak D-Wing <avehwing@gmail.com>
2019-06-07 21:24:15 +0800

    Update EDA.R
    remove "doubled" booking ID from label and grabData

--- EDA.R
+++ EDA.R
@@ -18,5 +18,43 @@ grabData <- readAll("features")
 # load all labels (0: non-dangerous driving, 1: dangerous driving)
 labelDF <- read_csv("./labels/part-00000-e9445087-aa0a-433b-a7f6-7f4c19d78ad6-c000.csv")
 
+# a list of booking ID with two labels (0 and 1) at the same time
 label_doubled <- labelDF %>% count(bookingID) %>% filter(n == 2) %>% pull(bookingID)
+
+# it is unknown why a trip consists of both labels.
+# for the ease of analysis, this booking ID is removed from analysis
+
+# remove the "doubled" bookingID from label
+labelDF_cleaned <-
+  labelDF %>% filter(!bookingID %in% label_doubled)
+
+# remove the "doubled" bookingID from grabData
+grabData_cleaned <-
+  grabData %>% filter(!bookingID %in% label_doubled)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
