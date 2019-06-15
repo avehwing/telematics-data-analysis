1ce4451c16adc71de4c280703376320add904d60
Kak D-Wing <avehwing@gmail.com>
2019-06-15 13:02:03 +0800

    Update data_preparing.R
    added bandpass filtered function to run on sensors data

--- data_preparing.R
+++ data_preparing.R
@@ -1,13 +1,64 @@
 library(tidyverse)
+library(signal)
+library(zoo)
 
 # load all preprocessed data
 load("./preprocessed.RData")
 
-# test band pass filter
-testing <- grabData_train %>% filter(bookingID == grabData_train$bookingID[100]) %>% 
-  select(acceleration_z, second) %>% arrange(second)
+# first attempt for data preparation
+# accelerometer data and GPS speed is used for the prediction
+
+grabData_train_1 <- 
+  grabData_train %>% 
+  select(bookingID, contains("acceleration"), second, Speed) %>% 
+  group_by(bookingID) %>% 
+  arrange(bookingID, second) %>% 
+  ungroup()
+
+# unique id for each trip
+ids <- unique(grabData_train_1$bookingID)
+
+# apply butterworth filter (bandpass filter) on all selected data
+
+bw <- butter(2, 0.1, type = "pass", plane = "z")
+
+# a function to apply bandpass filter on sensor data
+fil_fun <- function(ids, column_name){
+  filtered <- NULL
+  for(id in ids){
+    df <- grabData_train_1[grabData_train_1$bookingID == id, column_name]
+    fil <- filtfilt(bw, df[[column_name]])
+    filtered <- c(filtered, fil)
+  }
+  return(filtered)
+}
+
+# visualize the effect of  band pass filter
+testing <- grabData_train_1 %>% dplyr::filter(bookingID == grabData_train_1$bookingID[1]) 
 
 ggplot(data = testing)+
-  geom_line(aes(x = second, y = acceleration_z))
+  geom_line(aes(x = second, y = acceleration_z)) +
+  geom_line(aes(x = second, y = acc_z_filtered), color = "green") +
+  xlab("Time (seconds)") +
+  ylab("Acceleration") 
+
+# apply bandpass filter on acceleration and speed data
+
+column_names <- names(grabData_train_1)[c(2,3,4,6)]
+new_col <- c("acc_x_filtered", "acc_y_filtered", "acc_z_filtered", "Speed_filtered")
+
+system.time(for(j in 1:length(new_col)){
+  grabData_train_1[new_col[j]] <- fil_fun(ids, column_names[j])
+})
+
+
+
+
+
+
+
+
+
+
 
 
