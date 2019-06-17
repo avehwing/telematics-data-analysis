346f557047e741fbc0faaea2a28f4c7c6ac89e69
Kak D-Wing <avehwing@gmail.com>
2019-06-17 16:47:35 +0800

    Update holdOutTest.R
    Added the full process to perform hold out test

--- holdOutTest.R
+++ holdOutTest.R
@@ -1,7 +1,17 @@
 library(tidyverse)
+library(caret)
 library(signal)
 library(zoo)
 
 source("./preprocessing.R")
 
-grabData_dev_processed <- preprocessing_stage_1(grabData_dev)
+# load model
+model_rf <- readRDS("./model_rf.rds")
+
+# load test set and label
+test_DF <- read_csv("./")
+label_DF <- read_csv("./")
+
+# load result
+result <- hold_out_test(test_DF, label_DF, model_rf)
+
